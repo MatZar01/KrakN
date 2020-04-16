46092c22eb15813cf3aff1d034e406ac5e5963d9
Mateusz Żarski <zski.mateusz@gmail.com>
2020-04-16 12:42:04 +0200

    Create install_dependencies.bat

---
+++ install_dependencies.bat
@@ -0,0 +1,9 @@
+pip3 install tensorflow == 1.12.0
+pip3 install keras == 2.2.4
+pip3 install scikit-learn == 0.22.1
+pip3 install numpy == 1.16.2
+pip3 install opencv_python == 4.0.0
+pip3 install h5py == 2.9.0
+pip3 install progressbar2 == 2.5
+pip3 install imutils == 0.5.2
+pip3 install pygame == 1.9.6
