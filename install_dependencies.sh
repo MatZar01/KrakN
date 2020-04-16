8dfe21a148546a852d8fb365ce290b8b9c75171d
Jarek Miszczak <miszczak@iitis.pl>
2020-04-16 18:30:25 +0200

    Fixed helper scripts

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,9 +1,11 @@
-pip3 install tensorflow == 1.12.0
-pip3 install keras == 2.2.4
-pip3 install scikit-learn == 0.22.1
-pip3 install numpy == 1.16.2
-pip3 install opencv_python == 4.0.0
-pip3 install h5py == 2.9.0
-pip3 install progressbar2 == 2.5
-pip3 install imutils == 0.5.2
-pip3 install pygame == 1.9.6
+#!/bin/sh
+pip3 install tensorflow==1.12.0
+pip3 install keras==2.2.4
+pip3 install scikit-learn==0.22.1
+pip3 install numpy==1.16.2
+pip3 install opencv_python>=4.0.0
+pip3 install h5py==2.9.0
+pip3 install progressbar2==2.5
+pip3 install imutils==0.5.2
+pip3 install pygame==1.9.6
+pip3 install pillow==7.1.1
